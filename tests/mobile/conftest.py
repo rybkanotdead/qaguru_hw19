aade0a658bd77f886ba288c75a01209fc839c7b5
Alina Oganina <ifoxspirit@yandex.ru>
2025-04-16 19:09:16 +0500

    conftest

--- tests/mobile/conftest.py
+++ tests/mobile/conftest.py
@@ -16,7 +16,7 @@ def init_app_session(options):
             'http://hub.browserstack.com/wd/hub',
             options=options
         )
-    browser.config.timeout = float(os.getenv('timeout', '10.0'))
+    browser.config.timeout = float(os.getenv('timeout', '30.0'))
     browser.config._wait_decorator = support._logging.wait_with(
         context=allure_commons._allure.StepContext
     )
